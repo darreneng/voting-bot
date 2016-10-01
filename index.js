ed983bd84e8a6389defcb263fafc74081dbe979e
Darren Eng <darrenle9@gmail.com>
2016-10-01 00:36:38 -0700

    facebook webhook

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Server frontpage
+app.get('/', (req, res) => {
+    res.send('This is TestBot Server')
+})
+
+// Facebook webhook
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === 'poop') {
+    res.send(req.query['hub.challenge'])
+  } else {
+    res.send('Invalid verify token')
+  }
+})
+
+app.listen((process.env.PORT || 3000))
